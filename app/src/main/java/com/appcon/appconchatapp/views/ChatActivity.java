4c1d0e4d861a9c3bf4587cdfec1a2b571fa71004
ShehriyarShariq <shehriyarshariq@gmail.com>
2019-10-19 11:19:20 +0500

    Add Databing to Chat Activity

--- app/src/main/java/com/appcon/appconchatapp/views/ChatActivity.java
+++ app/src/main/java/com/appcon/appconchatapp/views/ChatActivity.java
@@ -1,16 +1,23 @@
 package com.appcon.appconchatapp.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
 import com.appcon.appconchatapp.R;
+import com.appcon.appconchatapp.databinding.ActivityChatBinding;
 
 public class ChatActivity extends AppCompatActivity {
+
+
+    ActivityChatBinding activityChatBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+
     }
 }
